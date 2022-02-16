d2b1fb36eed448b490eba55d8db9d9f688c1f999
Ilie Danila <ilie.danila@arnia.ro>
2022-02-16 14:12:11 +0200

    Updates

--- cubrid_commands.sh
+++ cubrid_commands.sh
@@ -24,6 +24,30 @@ function cubrid_config {
     popd
 }
 
+function cubrid_config_release {
+    if [ -z "$ARNIADB" ]
+    then
+        echo "env variable 'ARNIADB' was not found; nothing to do; bailing out ..."
+        exit -1
+    fi
+    
+    local build_dir=${ARNIADB}/../build
+
+    if [ -d ${build_dir} ] 
+    then
+        echo "Build directory exists. Moving on..." 
+    else
+        echo "Build directory does not exist. Creating..."
+        mkdir ${build_dir}
+    fi
+
+    pushd ${build_dir}
+
+    CC=gcc-8 CXX=g++-8 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${ARNIADB} ../repo
+
+    popd
+}
+
 function cubrid_build {
     if [ -z "$ARNIADB" ]
     then
